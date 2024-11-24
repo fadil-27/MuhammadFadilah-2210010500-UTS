36d1fe56ef7bf194f238dab28a9617615ad710fb
fadil-27 <fadilahmuhammad237@gmail.com>
2024-11-24 15:15:20 +0800

    Membuat JFrame

---
+++ AplikasiBukuAlamat/src/main/java/com/mycompany/aplikasibukualamat/AplikasiBukuAlamat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasibukualamat;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AplikasiBukuAlamat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
